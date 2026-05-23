9da25544bf46a3bc9ef87d36ddb8094ed770b620
arshitchoubey18 <arshitchoubey18@users.noreply.github.com>
2026-05-23 18:49:31 +0000

    Switch back to javax.servlet for Tomcat 9

--- src/main/java/com/insured/HelloServlet.java
+++ src/main/java/com/insured/HelloServlet.java
@@ -1,12 +1,12 @@
 package com.insured;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        resp.getWriter().println("<h2>Insured Assurance - Policy Service Running</h2>");
+        resp.getWriter().println("<h1>Insured Assurance - Hello from Jenkins!</h1>");
     }
 }
